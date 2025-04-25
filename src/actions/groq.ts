de0f6c38a81ed74ec375e72cef826762020d1f60
neuronas <orlandonuske@gmail.com>
2025-04-24 21:09:55 -0300

    Move actions outside app folder

--- src/actions/groq.ts
+++ src/actions/groq.ts
@@ -1,4 +1,3 @@
-// app/actions/groq.ts
 'use server';
 
 import { createGroq } from '@ai-sdk/groq';
@@ -9,10 +8,6 @@ const groq = createGroq({
 });
 
 export async function callGroq(prompt: string) {
-//   const result = await groq.chat.completions.create({
-//     model: 'mixtral-8x7b-32768',
-//     messages: [{ role: 'user', content: prompt }],
-//   });
   const result = await generateText({
     model: groq('qwen-qwq-32b'),
     providerOptions: {
