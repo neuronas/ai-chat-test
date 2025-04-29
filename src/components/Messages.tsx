385507a7dcc2586a18743e4599fad309a191e5a8
neuronas <orlandonuske@gmail.com>
2025-04-29 17:05:34 -0300

    Add Markdown component

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -20,19 +20,19 @@ export function Messages(
       <div className="text-sm/6 font-[family-name:var(--font-geist-mono)]">
         {msgHistory.map((item, k) => {
           if (item.role === "user") {
-            return (<div key={k} className="flex text-right flex-row-reverse">
-              <div key={k} className="flex border border-zinc-300 bg-gray-300 rounded-xl mb-2 p-3">
-                <span>
+            return (<div key={k} className="flex flex-col text-right flex-row-reverse">
+              <div key={k} className="flex flex-col border border-zinc-300 bg-gray-300 rounded-xl mb-2 p-3">
+                <Markdown>
                   {item.content}
-                </span>
+                </Markdown>
               </div>
             </div>)
           } else {
-            return (<div key={k} className="flex">
-              <div key={k} className="flex mb-2 p-3">
-                <span>
+            return (<div key={k} className="flex flex-col">
+              <div key={k} className="flex flex-col mb-2 p-3">
+                <Markdown>
                   {item.content}
-                </span>
+                </Markdown>
               </div>
             </div>
             )
@@ -52,5 +52,105 @@ export function Messages(
         }
       </div>
     </div>
+  )
+}
+
+import ReactMarkdown, { type Components } from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+const components: Partial<Components> = {
+  pre: ({ children }) => <>{children}</>,
+  ol: ({ children, ...props }) => {
+    return (
+      <ol className="list-decimal list-outside ml-4" {...props}>
+        {children}
+      </ol>
+    );
+  },
+  li: ({ children, ...props }) => {
+    return (
+      <li className="py-1" {...props}>
+        {children}
+      </li>
+    );
+  },
+  ul: ({ children, ...props }) => {
+    return (
+      <ul className="list-decimal list-outside ml-4" {...props}>
+        {children}
+      </ul>
+    );
+  },
+  strong: ({ children, ...props }) => {
+    return (
+      <span className="font-semibold" {...props}>
+        {children}
+      </span>
+    );
+  },
+  a: ({ children, ...props }) => {
+    return (
+      // @ts-expect-error
+      <Link
+        className="text-blue-500 hover:underline"
+        target="_blank"
+        rel="noreferrer"
+        {...props}
+      >
+        {children}
+      </Link>
+    );
+  },
+  h1: ({ children, ...props }) => {
+    return (
+      <h1 className="text-3xl font-semibold mt-6 mb-2" {...props}>
+        {children}
+      </h1>
+    );
+  },
+  h2: ({ children, ...props }) => {
+    return (
+      <h2 className="text-2xl font-semibold mt-6 mb-2" {...props}>
+        {children}
+      </h2>
+    );
+  },
+  h3: ({ children, ...props }) => {
+    return (
+      <h3 className="text-xl font-semibold mt-6 mb-2" {...props}>
+        {children}
+      </h3>
+    );
+  },
+  h4: ({ children, ...props }) => {
+    return (
+      <h4 className="text-lg font-semibold mt-6 mb-2" {...props}>
+        {children}
+      </h4>
+    );
+  },
+  h5: ({ children, ...props }) => {
+    return (
+      <h5 className="text-base font-semibold mt-6 mb-2" {...props}>
+        {children}
+      </h5>
+    );
+  },
+  h6: ({ children, ...props }) => {
+    return (
+      <h6 className="text-sm font-semibold mt-6 mb-2" {...props}>
+        {children}
+      </h6>
+    );
+  },
+};
+
+const remarkPlugins = [remarkGfm];
+
+function Markdown({ children }: { children: string }) {
+  return (
+    <ReactMarkdown remarkPlugins={remarkPlugins} components={components}>
+      {children}
+    </ReactMarkdown>
   )
 }
